0b57450777ea70c942be11537382053376bc1292
Nathaniel <stoddard.nathaniel@yahoo.com>
2025-05-14 13:08:07 -0500

    updates

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -10,8 +10,8 @@ declare module 'next-auth' {
       organization?: {
         id: string;
         name: string;
-        website?: string;
-        industry?: string;
+        website: string | null;
+        industry: string | null;
       };
     } & DefaultSession['user'];
   }
